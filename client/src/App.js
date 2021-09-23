5789402e1f7f1990f5afb6712b80a1900f7967c2
aghosh0605 <aghosh0605@gmail.com>
2021-09-23 11:04:16 +0530

    💩 UI added with bad code

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
